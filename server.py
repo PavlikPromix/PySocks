ede206f4e3024bf7ebe5f388d1ba5c50c569d6d7
PavlikPromix <beriya2004@mail.ru>
2021-01-11 01:26:54 +0300

    Created server side app

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket #importing socket lib
+
+soc = socket.socket() #creating socket() object
+soc.bind(('', 9090)) #binding our server to sertain type of connection (argument 1) and port (argiment 2)
+soc.listen(1) #starting to listen port for incoming connections
+conn, addr = soc.accept() #accepting incoming connections; method .accept() returns new socket (conn) and client's address (addr)
+
+print(addr + " connected")
+
+while True:
+    data = conn.recv(1024) #receiving 1Kb of data
+    if not data:
+        break
+    conn.send(data.upper()) #sending answer
+conn.close() #closing connection
