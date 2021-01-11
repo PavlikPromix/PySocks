49f92a714f7d57d2ce9a531d8df66a775567dc49
PavlikPromix <beriya2004@mail.ru>
2021-01-11 17:31:09 +0300

    Fixed bytes convertation

--- client.py
+++ client.py
@@ -2,9 +2,9 @@ import socket
 
 sock = socket.socket() #creating socket() obj
 sock.connect(('localhost', 9090)) #connecting to 'localhost' with port 9090
-sock.send(input("$ ")) #sending message to 'localhost'
+sock.send(bytes(input("$ "), 'utf-8')) #sending message to 'localhost'
 
 data = sock.recv(1024) #receiving message from 'localhost'
 sock.close() #closing connection
 
-print (data) 
+print (data.decode('utf-8')) 
--- server.py
+++ server.py
@@ -5,11 +5,12 @@ soc.bind(('', 9090)) #binding our server to sertain type of connection (argument
 soc.listen(1) #starting to listen port for incoming connections
 conn, addr = soc.accept() #accepting incoming connections; method .accept() returns new socket (conn) and client's address (addr)
 
-print(addr + " connected")
+print(addr, " connected")
 
 while True:
     data = conn.recv(1024) #receiving 1Kb of data
+    print (data.decode('utf-8'))
     if not data:
         break
-    conn.send(data.join(bytes(" - accepted", 'utf-8'))) #sending answer
+    conn.send(data.upper()) #sending answer
 conn.close() #closing connection
